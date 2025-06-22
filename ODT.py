e38afeb99b8da8d7718d4616de2e86c45b24f23b
Unknown16384 <kuromizu@ya.ru>
2025-06-22 11:04:19 +0000

    Update ODT.py

--- ODT.py
+++ ODT.py
@@ -1,5 +1,5 @@
 from session import Connection
-from main import Products, Suppliers
+from main import Products
 from pydantic import BaseModel
 
 class ODT(BaseModel):
@@ -10,5 +10,5 @@ connect = Connection(sql_type='SQLite', db_name='mydb')
 engine = connect.engine
 session = connect.session
 
-for row in session.query(Products):
+for row in session.query(Products).all():
     print(ODT.model_validate(row, from_attributes=True))
